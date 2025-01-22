697181495ce6f5f30cb893be724a5a4c65f26261
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-22 15:27:01 +0300

    adds main.go

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"example/pdfgenerator/controllers"
+	"example/pdfgenerator/initializers"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+	initializers.MigrateDB()
+	initializers.InitMinioClient()
+}
+
+func main() {
+
+	r := gin.Default()
+
+	// Set up CORS middleware
+	config := cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}
+	r.Use(cors.New(config))
+
+	r.POST("/upload-template", controllers.UploadTemplate)
+	r.POST("/generate", controllers.CreateDocument, controllers.AutodocsLogs)
+	r.GET("/documents", controllers.GetDocuments)
+	r.GET("/templates", controllers.Templates)
+	r.GET("/document-history", controllers.GetDocumentHistory)
+	r.GET("/logs", controllers.AutodocsLogs)
+	r.GET("/daterange-metrics", controllers.GetRangeMetrics)
+	r.GET("failed-generations", controllers.GetFailedGenerations)
+
+	r.GET("/templates/preview/:refNumber", controllers.PreviewTemplate)
+	r.GET("/documents/preview/:refNumber", controllers.PreviewDocument)
+
+	r.DELETE("/templates/:refNumber", controllers.DeleteTemplate)
+	r.DELETE("/documents/:refNumber", controllers.DeleteDocument)
+	r.DELETE("/clear-logs", controllers.DeleteAllLogs)
+
+	//endpoint to log the html before it turns to pdf
+	r.POST("/htmlbeforepdf", controllers.HtmlBeforePDF)
+	r.Run()
+}
