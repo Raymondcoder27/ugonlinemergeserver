944f8557cc7b574e9b97af1c8d23da4844518ac4
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-30 15:36:41 +0300

    adds transaction id to admin agent

--- models/backofficeAccount.go
+++ models/backofficeAccount.go
@@ -6,7 +6,7 @@ type BackofficeAccount struct {
 	FirstName string `json:"firstname" gorm:"unique;"`
 	// MiddleName  string `json:"middleName" gorm:""`
 	LastName    string `json:"lastName" gorm:""`
-	PhoneNumber string `json:"phoneNumber" gorm:"unique;"`
+	PhoneNumber string `json:"phone" gorm:"unique;"`
 	Email       string `json:"email" gorm:"unique;"`
 	Role        string `json:"role" gorm:""`   // e.g., "Administrator", "Manager"
 	Till        string `json:"till" gorm:""`   // e.g., "Till 1"
