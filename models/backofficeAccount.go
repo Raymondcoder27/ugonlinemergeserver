6ba900f8898c9dff62ac03e92acaae64782ddefb
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-30 15:39:47 +0300

    adds transaction id

--- models/backofficeAccount.go
+++ models/backofficeAccount.go
@@ -3,12 +3,12 @@ package models
 // BackofficeAccount represents a user who manages back-office operations.
 type BackofficeAccount struct {
 	ID        uint   `json:"id" gorm:"primaryKey"`
-	FirstName string `json:"firstname" gorm:"unique;"`
+	FirstName string `json:"firstName" gorm:"unique;"`
 	// MiddleName  string `json:"middleName" gorm:""`
-	LastName    string `json:"lastName" gorm:""`
-	PhoneNumber string `json:"phone" gorm:"unique;"`
-	Email       string `json:"email" gorm:"unique;"`
-	Role        string `json:"role" gorm:""`   // e.g., "Administrator", "Manager"
-	Till        string `json:"till" gorm:""`   // e.g., "Till 1"
-	Status      string `json:"status" gorm:""` // e.g., "Active", "Inactive"
+	LastName string `json:"lastName" gorm:""`
+	Phone    string `json:"phone" gorm:"unique;"`
+	Email    string `json:"email" gorm:"unique;"`
+	Role     string `json:"role" gorm:""`   // e.g., "Administrator", "Manager"
+	Till     string `json:"till" gorm:""`   // e.g., "Till 1"
+	Status   string `json:"status" gorm:""` // e.g., "Active", "Inactive"
 }
