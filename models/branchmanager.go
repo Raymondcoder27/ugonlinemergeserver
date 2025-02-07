91d3445f9c0cc9047f7bb3b92178481ec10e14e6
RaymondCoder27 <mwebraymond13@gmail.com>
2025-02-07 11:52:32 +0300

    adds transaction id

--- models/branchmanager.go
+++ models/branchmanager.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 // TillOperator represents a user managing float at specific tills.
 type TillOperator struct {
 	ID string `json:"id" gorm:"primaryKey"`
@@ -10,4 +12,14 @@ type TillOperator struct {
 	Role      string `json:"role" gorm:"not null"`   // e.g., "Manager"
 	Till      string `json:"till" gorm:"not null"`   // e.g., "Till 1"
 	Status    string `json:"status" gorm:"not null"` // e.g., "Active", "Inactive"
+}
+
+type BranchManagerFloatLedger struct {
+	ID          string    `json:"id" gorm:"primaryKey"`
+	Description string    `json:"description" gorm:"not null"`
+	Amount      float64   `json:"amount" gorm:"not null"` // The provider of the service
+	Balance     float64   `json:"balance" gorm:"not null"`
+	Status      string    `json:"status" gorm:"not null"` // e.g., "Active", "Inactive"
+	CreatedAt   time.Time `json:"createdAt" gorm:"autoCreateTime"`
+	UpdatedAt   time.Time `json:"updatedAt" gorm:"autoUpdateTime"`
 }
